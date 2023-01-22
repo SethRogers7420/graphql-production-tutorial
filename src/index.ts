8d49887f90d3a325fa6b303b496b40df0f0cc462
Seth Rogers <Seth.Rogers7420@gmail.com>
2023-01-22 16:11:17 -0500

    Running apollo server

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import { ApolloServer } from "apollo-server";
-import { loadGraphQLSchema } from "src/services/graphql/graphql";
-import { createContext } from "src/services/graphql/context";
+import { loadGraphQLSchema } from "src/graphql/schema";
 
 const schema = loadGraphQLSchema();
-new ApolloServer({ schema, context: createContext })
+new ApolloServer({ schema, persistedQueries: false })
   .listen({ port: 9000 })
   .then(({ server, url }) => {
     // This must be higher than the idle timeout value set on the load balancer, otherwise
