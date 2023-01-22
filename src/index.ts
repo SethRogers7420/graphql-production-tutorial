eff8c3a9aca52883b27dfc77f93410c4ced9367b
Seth Rogers <Seth.Rogers7420@gmail.com>
2023-01-22 16:12:53 -0500

    Comment.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import { ApolloServer } from "apollo-server";
 import { loadGraphQLSchema } from "src/graphql/schema";
 
+// Load the schema from our .graphql files.
 const schema = loadGraphQLSchema();
+
 new ApolloServer({ schema, persistedQueries: false })
-  .listen({ port: 9000 })
+  .listen({ port: 8000 })
   .then(({ server, url }) => {
-    // This must be higher than the idle timeout value set on the load balancer, otherwise
-    // we may see intermittent 502 errors from the load balancer
+    // This is a workaround for problems with an application load balancer in AWS where intermittent 502 errors from NGINX are returned.
+    //
+    // This must be higher than the idle timeout value set on the load balancer which defaults to 60 seconds.
     server.keepAliveTimeout = 65 * 1000; // in milliseconds
 
     console.log(`🚀 Server ready at ${url}`);
